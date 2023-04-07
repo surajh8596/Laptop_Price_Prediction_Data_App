22b8d583ea53d4399886b40e2ab7b8d7b2973e2e
Suraj Honkamble <104256430+surajh8596@users.noreply.github.com>
2023-04-07 16:05:04 +0530

    Add files via upload

--- laptop_price_prediction/pages/1_Relation_Price_Feature.py
+++ laptop_price_prediction/pages/1_Relation_Price_Feature.py
@@ -24,5 +24,41 @@ feature=st.selectbox(
     "Select Feature to See the Relation",
     (data.columns))
 
-fig = px.box(df, x=feature, y='MRP', color=feature)
+#Create relationship Bar plot for min max and average MRP for selected feature
+aggregate=df.groupby(feature).agg({"MRP":{"min", "max", "mean"}})
+aggregate.columns= ["_".join(col) for col in aggregate.columns]
+aggregate= aggregate.reset_index()
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=aggregate[feature],
+                         y=aggregate["MRP_min"],
+                         name="Minimum Price",
+                         mode="markers",
+                         showlegend=True,
+                         marker=dict(color="red", size=8)))
+
+fig.add_trace(go.Scatter(x=aggregate[feature],
+                         y=aggregate["MRP_mean"],
+                         name="Average Price",
+                         mode="markers",
+                         showlegend=True,
+                         marker=dict(color="green",size=15)))
+
+fig.add_trace(go.Scatter(x=aggregate[feature],
+                         y=aggregate["MRP_max"],
+                         name="Maximum Price",
+                         mode="markers",
+                         showlegend=True,
+                         marker=dict(color="blue",size=20)))
+#Add vertical line to differentiate category
+for i, row in aggregate.iterrows():
+    if row["MRP_min"]!=row["MRP_max"]:
+        fig.add_shape(
+            dict(type="line",
+                 x0=row[feature],
+                 x1=row[feature],
+                 y0=row["MRP_min"],
+                 y1=row["MRP_max"],
+                 line=dict(color="pink",width=0.5)))
+fig.update_layout(title="Average, Minimum and Maximum Laptop Price by {}".format(feature))
 st.plotly_chart(fig, use_container_width=True)
